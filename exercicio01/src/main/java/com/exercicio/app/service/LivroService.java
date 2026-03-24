f19ffe0f3ae93cb4e870c8b648cd39acb9079481
Your Name <you@example.com>
2026-03-24 18:36:19 -0300

    services

---
+++ exercicio01/src/main/java/com/exercicio/app/service/LivroService.java
@@ -0,0 +1,32 @@
+package com.exercicio.app.service;
+import com.exercicio.app.entity.Livro;
+import com.exercicio.app.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    private final LivroRepository repository;
+
+    public LivroService(LivroRepository repository) {
+        this.repository = repository;
+    }
+
+    public Livro salvar(Livro livro) {
+        return repository.save(livro);
+    }
+
+    public List<Livro> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Livro> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
