b3a0230162ac67629b0dc246107ea51152d3fe60
Your Name <you@example.com>
2026-03-24 18:40:38 -0300

    services

---
+++ exercicio03/src/main/java/com/exercicio/app/service/PedidoService.java
@@ -0,0 +1,21 @@
+package com.exercicio.app.service;
+import com.exercicio.app.entity.Pedido;
+import com.exercicio.app.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    private final PedidoRepository repository;
+
+    public PedidoService(PedidoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Pedido salvar(Pedido pedido) { return repository.save(pedido); }
+    public List<Pedido> listarTodos() { return repository.findAll(); }
+    public Optional<Pedido> buscarPorId(Long id) { return repository.findById(id); }
+    public void deletar(Long id) { repository.deleteById(id); }
+}
