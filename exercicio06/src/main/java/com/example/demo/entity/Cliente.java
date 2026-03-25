c8a6674007dbac5156db34fb2ad737b1c0b81fa0
Gabriel Cesar Gravena <spfcgabreu@gmail.com>
2026-03-24 23:40:53 -0300

    entity cliente

---
+++ exercicio06/src/main/java/com/example/demo/entity/Cliente.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nome_cliente")
+	private String nome;
+
+	@Column(name = "email_cliente")
+	private String email;
+
+	@Column(name = "telefone_cliente")
+	private String telefone;
+
+	// Getters e Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
