1e778dc60c8ec290e10aea168ac8bb9d2b51c2c8
Gabriel Cesar Gravena <spfcgabreu@gmail.com>
2026-03-24 23:57:46 -0300

    entity tarefa

---
+++ exercicio10/src/main/java/com/example/demo/entity/Tarefa.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity(name = "Tarefa")
+public class Tarefa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "descricao_tarefa")
+	private String descricao;
+
+	@Column(name = "data_vencimento_tarefa")
+	private LocalDate dataVencimento;
+
+	@Column(name = "concluida_tarefa")
+	private Boolean concluida;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(LocalDate dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Boolean getConcluida() {
+		return concluida;
+	}
+
+	public void setConcluida(Boolean concluida) {
+		this.concluida = concluida;
+	}
+}
